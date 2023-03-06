8fd75277334cd1f0eb6aa7860e160ee0c0051fc8
azard285 <124109803+azard285@users.noreply.github.com>
2023-03-07 01:48:56 +0700

    то что нужно

---
+++ per.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *init(int n)
+{
+    int *arr = malloc(n * sizeof(int));
+    int i;
+
+    for (i = 0; i < n; ++i)
+    {
+        arr[i] = i;
+    }
+
+    return arr;
+}
+
+int main()
+{
+    int* arr = NULL;
+    int n = 10;
+    arr = init(n);
+    int i;
+
+    for (i = 0; i < n; ++i)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+    }
